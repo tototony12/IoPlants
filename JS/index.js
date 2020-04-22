ba12d5f3c960761deaeeb1c6fa06e2f536efa09a
Diego Ascencio <is698355@iteso.mx>
2020-04-21 20:39:56 -0500

    Front end almost done

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,12 @@
 localStorage.sessionId;
 localStorage.sessionEmail;
 localStorage.userId;
+localStorage.plantName;
 
+let imagenPlanta = document.getElementById('imagenPlanta');
+let graficas = document.getElementById('graficas');
+let nombrePlanta = document.getElementById('nombrePlanta');
+let img = document.getElementById('img');
 
 //VALIDAR USUARIO LOGUEADO
 function onLoad() {
@@ -22,3 +27,23 @@ function signOut() {
 }
 
 signout.addEventListener("click", signOut);
+
+function jitomate(){
+    nombrePlanta.textContent = "Jitomate"
+    localStorage.plantName = "jitomate";
+    img.src= "https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2017/02/jitomate3.jpg"
+    imagenPlanta.style.display = '';
+    graficas.style.display = '';
+}
+function lechuga(){
+    localStorage.plantName = "lechuga";
+    imagenPlanta.style.display = '';
+    img.src = "https://biotrendies.com/wp-content/uploads/2015/07/lechuga-1000x600.jpg";
+    graficas.style.display = 'none';
+}
+function cebolla(){
+    localStorage.plantName = "cebolla";
+    imagenPlanta.style.display = '';
+    img.src = "http://geneseeds.com.mx/wp-content/uploads/Cebolla-Crystal-White-Wax.png";
+    graficas.style.display = 'none';
+}
